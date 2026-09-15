68dfadbc712be3f0b493c05790ae83e6a42a24c6
zubari666 <155691678+zubari666@users.noreply.github.com>
2026-09-15 21:14:14 +0300

    Update YouTubeLinkGrabber.py

--- YouTubeLinkGrabber.py
+++ YouTubeLinkGrabber.py
@@ -5,7 +5,7 @@ import os
 def grab(url):
     response = requests.get(url, timeout = 15).text
     if '.m3u8' not in response:
-        print("https://www.youtube.com/watch?v=1oh9IEwBbFY")
+        print("https://www.youtube.com/watch?v=hHqtlnLNNtI&list=RDbbKrlb8clxc&index=3")
         return
     end = response.find('.m3u8') + 5
     tuner = 100
